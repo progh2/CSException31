ea4bc7aacc2b3c0d8c17ab59807a613c4d2d8453
Gihun Ham <ham@e-mirim.hs.kr>
2024-09-25 09:44:01 +0900

    예외 처리 - 예외 발생

--- CSException31/Program.cs
+++ CSException31/Program.cs
@@ -8,8 +8,56 @@ namespace CSException31
 {
     internal class Program
     {
+
+        class Box
+        {
+            private int width;
+            public int Width
+            {
+                get { return width; }
+                set
+                {
+                    if (value <= 0)
+                        throw new Exception("너비는 0보다 커야 합니다.");
+                    width = value;
+                }
+            }
+
+            private int height;
+            public int Height
+            {
+                get { return height; }
+                set
+                {
+                    if (value <= 0)
+                        throw new Exception("높이는 0보다 커야 합니다.");
+                    height = value;
+                }
+            }
+
+            public Box(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            // 박스의 넓이를 계산하는 속성
+            public int Area
+            {
+                get
+                {
+                    return width * height;
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            Box b1 = new Box(10, 10);
+            Console.WriteLine("Area: {0}", b1.Area);
+            b1.Width = 0;
+
         }
     }
 }
