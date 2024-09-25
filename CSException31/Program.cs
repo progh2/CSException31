9b5d3b0de3a834007fcc8db1956b01f6e33f8d45
Gihun Ham <ham@e-mirim.hs.kr>
2024-09-25 09:48:41 +0900

    사용자 정의 예외 클래스 정의 및 사용, 다중 블럭 catch

--- CSException31/Program.cs
+++ CSException31/Program.cs
@@ -8,6 +8,19 @@ namespace CSException31
 {
     internal class Program
     {
+        class BoxWrongWidthException : Exception
+        {
+            public BoxWrongWidthException(string message) : base(message)
+            {
+            }
+        }
+
+        class BoxWrongHeightException : Exception
+        {
+            public BoxWrongHeightException(string message) : base(message)
+            {
+            }
+        }
 
         class Box
         {
@@ -18,7 +31,7 @@ namespace CSException31
                 set
                 {
                     if (value <= 0)
-                        throw new Exception("너비는 0보다 커야 합니다.");
+                        throw new BoxWrongWidthException("너비는 0보다 커야 합니다.");
                     width = value;
                 }
             }
@@ -30,7 +43,7 @@ namespace CSException31
                 set
                 {
                     if (value <= 0)
-                        throw new Exception("높이는 0보다 커야 합니다.");
+                        throw new BoxWrongHeightException("높이는 0보다 커야 합니다.");
                     height = value;
                 }
             }
@@ -61,6 +74,14 @@ namespace CSException31
                 Console.WriteLine("Area: {0}", b1.Area);
                 b1.Width = 0;
             }
+            catch(BoxWrongWidthException e)
+            {
+                Console.WriteLine("아 쫌 너비값 자연수!!");
+            }
+            catch (BoxWrongHeightException e)
+            {
+                Console.WriteLine("아 쫌 높이값 자연수!!");
+            }   
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
